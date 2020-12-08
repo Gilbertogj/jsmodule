68554f022ace28e5811916ce146e126ca7c48c90
Gilbertogj <gilbertogj23@gmail.com>
2020-12-07 22:25:06 -0600

    ADD Introduction JS

---
+++ js/main.js
@@ -0,0 +1,22 @@
+console.log('Hola Koders')
+
+// variables var , Let , Const
+// let si va a cambiar mi valor
+// const si es un valor fijo
+// snake_case
+// Const
+const NAME_KODER = "oscar"
+// let
+let name_koder = prompt('¿Cuál es tu nombre?')
+// Case Styles
+// camelCase
+// PascalCase
+// snake_case
+
+console.log(name_koder)
+
+let age_koder=prompt('¿Cuál es tu edad')
+console.log(age_koder)
+
+let resultSuma= parseInt(age_koder)+5
+console.log('suma: ',resultSuma)
