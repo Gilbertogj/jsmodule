544bcf13de7cd822470bffbb70abab3c83ee2e8d
Gilbertogj <gilbertogj23@gmail.com>
2020-12-08 19:52:33 -0600

    add weight in moon

--- js/weight_in_moon.js
+++ js/weight_in_moon.js
@@ -5,4 +5,7 @@ let weight_earth = prompt("Introduce tu peso")
 let weight_moon = (parseFloat(weight_earth)/9.81)*1.622
 
 alert (weight_moon)
-console.log("Peso en la luna:", weight_moon)
+let result= 'Tu peso en la luna es: '+ weight_moon+'  kg'
+ let resultTwo=`Tu peso en la luna es: ${weight_moon} kg`
+onsole.log("Peso en la luna:", weight_moon)
+alert(resultTwo)
