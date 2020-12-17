1f7462c53fee2486eeb597f02dd48172c2195ffa
Gilbertogj <gilbertogj23@gmail.com>
2020-12-16 19:11:41 -0600

    homework

--- js/methods_array.js
+++ js/methods_array.js
@@ -48,6 +48,57 @@ function even(myArray2){
 // function
 // ['hola','mundo']
 // ->['ha','mo']
+function firsLast(myArray3){
+    let newArray3=[]    
+    myArray3.forEach(currentElement3 =>{
+        newArray3.push(`${currentElement3.slice(0,1)}${currentElement3.slice(-1)}`)
+        // o se puede 
+        // newAarray.push(currentElement3.slice(0,1)+currentElement3.slice(-1))
+    })
+    return newArray3
+}
+
+// map
+// utilizar el map cuando esperamos un arreglo de resultado
+[1,2,3].map((currentValue,index,arr)=>{
+    console.log(currentValue, index, arr)
+})
+
+// multiplicar los elementos del array por dos arrow 
+[1,2,3].map((currentValue)=>{
+    return currentValue*2
+})
+
+// o puede ser asi arrow functions
+[1,2,3].map((currentValue) =>currentValue*2)
+
+// con fucntions
+arr.map(function(currentValue,index,array){
+})
+
+
+// double con map creando fucntion
+
+const doubleValuesMap = (arr) =>{
+    let arrayResult =arr.map(currentValue => currentValue*2 )
+    return arrayResult
+}
+
+//  o 
+
+const doubleValuesMap = (arr) =>{
+    return arr.map(currentValue => currentValue*2 )
+}
+
+// o 
+
+const doubleValuesMap = (arr) => arr.map(currentValue => currentValue*2 )
+console.log(doubleValuesMap([1,2,3,4,5]))
+
+
+
+
+
 
 
 
